b500e60f1d60e6654dc48637add6d2d05c38f5f8
MonkeyDonkeyKing <lukas.bali15@gmail.com>
2023-10-29 10:59:34 +0100

    Finish Aufgabe 4

--- app/Aufgabe4.hs
+++ app/Aufgabe4.hs
@@ -5,52 +5,46 @@ module Aufgabe4 where
 
 -- Aufgabe 1
 -- Schreiben Sie eine Funktion, die alle pythagoreischen Zahlentripel bis zu
--- einer maximalen Gr"o"se n ausgibt. Achten Sie darauf, dass keine doppelten
+-- einer maximalen Größe n ausgibt. Achten Sie darauf, dass keine doppelten
 -- Tripel (z. B. (3,4,5) ~ (4,3,5) ) vorkommen. Verwenden Sie eine List
 -- Comprehension. Zur Erinnerung: \[ a^2 + b^2 = c^2 \].
 
--- > pythagoras :: Integer -> [(Integer, Integer, Integer)]
--- > pythagoras = undefined
-
+-- pythagoras :: Integer -> [(Integer, Integer, Integer)]
+-- pythagoras n = [(a, b, c) | a <- [1..n], b <- [a..n], c <- [b..n], a^2 + b^2 == c^2]
 
 
 -- Aufgabe 2
-
--- Schreiben Sie eine Funktion, die alle Quadrupel bis zu einer maximalen Gr"o"se ausgibt, die folgende Gleichung erf"ullen:
+-- Schreiben Sie eine Funktion, die alle Quadrupel bis zu einer maximalen Größe ausgibt, die folgende Gleichung erfüllen:
 -- \[ a^2 + b^2 = c^2 + d^2 \].
-
 -- Vermeiden Sie wie in der vorherigen Aufgabe Quadrupel, die im wesentlichen
 -- gleich sind.
 
--- > quadratsummen :: Integer -> [(Integer, Integer, Integer, Integer)]
--- > quadratsummen = undefined
-
--- \end{aufgabe}
-
--- \begin{aufgabe}
--- Schreiben Sie die Funktion replicate aus Data.List als List Comprehension.
-
--- > replicate' :: Int -> a -> [a]
--- > replicate' = undefined
+-- quadratsummen :: Integer -> [(Integer, Integer, Integer, Integer)]
+-- quadratsummen n = [(a, b, c, d) | a <- [1..n], b <- [a..n], c <- [b..n], d <- [c..n], a^2 + b^2 == c^2 + d^2]
 
 
 -- Aufgabe 3
+-- Schreiben Sie die Funktion replicate aus Data.List als List Comprehension.
 
+-- replicate' :: Int -> a -> [a]
+-- replicate' n x = [x | _ <- [1..n]]
+
+
+-- Aufgabe 4
 -- Formulieren Sie das Skalarprodukt zweier Vektoren, die als Liste implementiert
 -- sind. Verwenden Sie an geeignerter Stelle eine List Comprehension.
 
 -- \[ x * y = \sum_{i=0}^{n-1}(x_i * y_i) \]
 
 -- > skalar :: [Double] -> [Double] -> Double
--- > skalar = undefined
+-- > skalar x y = sum [ x_i * y_i | (x_i, y_i) <- zip x y]
 
 
--- Aufgabe 4
-
+-- Aufgabe 5
 -- Schreiben Sie die Funktion partition aus Data.List als List Comprehension.
 
--- > partition' :: (a -> Bool) -> [a] -> ([a], [a])
--- > partition' = undefined
+-- partition' :: (a -> Bool) -> [a] -> ([a], [a])
+-- partition' p xs = ([x | x <- xs, p x], [x | x <- xs, not (p x)])
 
 
 
