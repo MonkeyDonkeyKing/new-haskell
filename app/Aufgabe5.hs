8d82936bcff110bed73f34b07d0595037764a373
MonkeyDonkeyKing <lukas.bali15@gmail.com>
2023-11-02 14:46:05 +0100

    add aufgabe 5 (raw)

---
+++ app/Aufgabe5.hs
@@ -0,0 +1,48 @@
+module Aufgabe5 where
+
+-- \chapter{Rekursion}
+
+-- \begin{aufgabe}
+-- Definieren Sie unter Verwendung der Multiplikation (*) den
+-- Potenzierungsoperator als rekursive Funktion:
+
+-- > potenz :: Integer -> Integer -> Integer
+-- > potenz = undefined
+
+-- \end{aufgabe}
+
+-- \begin{aufgabe}
+-- Implementieren Sie den Euklidischen Algorithmus zur Bestimmung des gr"o"sten
+-- gemeinsamen Teilers als rekursive Funktion.
+
+-- > euklid :: Integer -> Integer -> Integer
+-- > euklid = undefined
+
+-- \end{aufgabe}
+
+-- \begin{aufgabe}
+-- Definieren Sie die Funktion `concat` der Standardbibliothek, die eine
+-- Liste von Listen zu einer Liste umwandelt, als rekursive Funktion.
+
+-- > konkat :: [[a]] -> [a]
+-- > konkat = undefined
+
+-- \end{aufgabe}
+
+-- \begin{aufgabe}
+-- Definieren Sie den (!!)-Operator, der das n-te Element einer Liste liefert
+-- als rekursive Funktion.
+
+-- > index :: [a] -> Int -> a
+-- > index = undefined
+
+-- \end{aufgabe}
+
+-- \begin{aufgabe}
+-- Definieren sie eine rekursive Funktion, die von einem gegebenen Element
+-- entscheidet, ob es sich in einer Liste befindet.
+
+-- > element :: Eq a => a -> [a] -> Bool
+-- > element = undefined
+
+-- \end{aufgabe}
